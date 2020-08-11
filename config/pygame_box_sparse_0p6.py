d51d99a3a21c55ec004d30c265bacfdec897c250
Vitchyr Pong <vitchyr@gmail.com>
2020-08-11 14:08:16 -0700

    fix sparse to actually be space

--- config/pygame_box_sparse_0p6.py
+++ config/pygame_box_sparse_0p6.py
@@ -5,14 +5,14 @@ from __future__ import print_function
 from config.pygame_base import PygameBoxWallsConfigModule
 
 
-class PygameDenseConfigModule(PygameBoxWallsConfigModule):
+class PygameBoxSparseConfigModule(PygameBoxWallsConfigModule):
     @staticmethod
     def obs_cost_fn(obs):
         positions = obs[:, :2]
         goals = obs[:, 2:]
         deltas = (positions - goals)
         distances = (deltas ** 2).sum(dim=1).sqrt()
-        return distances
+        return (distances >= 0.6).type(obs.dtype)
 
 
-CONFIG_MODULE = PygameDenseConfigModule
+CONFIG_MODULE = PygameBoxSparseConfigModule
